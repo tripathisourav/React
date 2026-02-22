67a38be05c9f5c79b173f2fde73c91a713c5b84e
Sourav Tripathi <sourabh12abh@gmail.com>
2026-02-22 17:22:05 +0530

    class4-again

--- class4/src/components/Woman.jsx
+++ class4/src/components/Woman.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Woman = () => {
   return (
     <div className='px-8 py-5 w-fit bg-pink-600 rounded text-xl font-semibold'>
-      <h1 text-xl font-bold>Women's Washroom</h1>
+      <h1 className='text-xl font-bold'>Women's Washroom</h1>
     </div>
   )
 }
